2b1e6d82751a460f1bedf7379930ca23504622c0
thuongnthe173210 <thuongnthe173210@fpt.edu.vn>
2026-01-21 23:19:04 +0700

    add fix

---
+++ src/main/java/com/assigment_2_thuongnt87/Assigment2ThuongNt87Application.java
@@ -0,0 +1,13 @@
+package com.assigment_2_thuongnt87;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assigment2ThuongNt87Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Assigment2ThuongNt87Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/assigment_2_thuongnt87/Assigment2ThuongNt87ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assigment_2_thuongnt87;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assigment2ThuongNt87ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
